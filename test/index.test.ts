15b68db7a33bf7b575606b73473bd264123a0024
Torben Haack <haack.t@icloud.com>
2024-03-15 08:07:32 +0100

    Add JSONCanvas tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,34 @@
 import { describe, expect, it } from 'vitest'
+import { JSONCanvas, GenericNode, TextNode, Edge, CanvasColor } from "../src/index";
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+
+// Create simple JSONCanvas, add a node and then export it to string
+describe('JSONCanvas', () => {
+  it('should create a simple JSONCanvas, add a node and then export it to string', () => {
+    // import { JSONCanvas, GenericNode, TextNode, Edge, CanvasColor } from "../src/index";
+    const canvas = new JSONCanvas();
+    const node: TextNode = {
+        id: "node1",
+        type: "text",
+        x: 100,
+        y: 100,
+        width: 100,
+        height: 50,
+        text: "Hello, world!",
+        color: CanvasColor.RED
+    };
+    canvas.add_node(node);
+    const result = canvas.to_string();
+    expect(result).toEqual('{"nodes":[{"id":"node1","type":"text","x":100,"y":100,"width":100,"height":50,"text":"Hello, world!","color":1}],"edges":[]}')
   })
-})
+});
+
+// Serialize 'Pale Sight.canvas' JSON Canvas to JSONCanvas
+describe('JSONCanvas', () => {
+  it('should serialize JSON Canvas to JSONCanvas object', () => {
+    const canvasString = JSON.stringify(require('./Canvas.json'));
+    const canvas = JSONCanvas.from_string(canvasString);
+    const result = canvas.to_string();
+    expect(result).toEqual(canvasString);
+  })
+});
